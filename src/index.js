41bbd00f8faa89f4bdc57c3344cd4acab78c0f91
Michael Angerman <stormasm@gmail.com>
2018-03-09 09:31:30 -0800

    pretty code first

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { onError } from 'apollo-link-error';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from "apollo-link-error";
 
-import './style.css';
+import "./style.css";
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 const httpLink = new HttpLink({
-  uri: 'https://api.github.com/graphql',
+  uri: "https://api.github.com/graphql",
   headers: {
-    authorization: 'bearer github-token-goes-here',
-  },
+    authorization: "bearer github-token-goes-here"
+  }
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.map(({ message, locations, path }) =>
       console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
     );
 
   if (networkError) console.log(`[Network error]: ${networkError}`);
@@ -34,19 +34,19 @@ const link = ApolloLink.from([errorLink, httpLink]);
 
 const cache = new InMemoryCache({
   logger: console.log,
-  loggerEnabled: true,
+  loggerEnabled: true
 });
 
 const client = new ApolloClient({
   link,
-  cache,
+  cache
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 registerServiceWorker();
